aeb639b6982ea997415e0b4845fcec84dfa8eadf
Aarii08 <140925809+Aarii08@users.noreply.github.com>
2023-12-04 08:47:12 +0000

    [Add]JS復習

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const el = document.querySelector('.animate-title');
+  const str = el.innerHTML.trim();
+
+  for(let c of str) {
+    console.log(`<span class="char">${c}</span>`)
+  }
+});
